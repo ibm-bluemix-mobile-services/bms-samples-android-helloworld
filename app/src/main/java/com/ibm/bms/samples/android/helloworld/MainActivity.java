7396d29858432504cad76ed7d14604d9644e3371
James Young <jmyoung@us.ibm.com>
2015-09-23 14:36:13 -0400

    Initial Hello World for Android
    Requires additional changes to the gradle settings to run as
    dependancies are not yet on public repos.

--- app/src/main/java/com/ibm/bms/samples/android/helloworld/MainActivity.java
+++ app/src/main/java/com/ibm/bms/samples/android/helloworld/MainActivity.java
@@ -1,26 +1,108 @@
-/*
- *     Copyright 2015 IBM Corp.
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
- */
+package com.ibm.helloworld_android;
 
-package com.ibm.bms.samples.android.helloworld;
-
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.BMSClient;
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.Response;
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.ResponseListener;
+import com.ibm.mobilefirstplatform.clientsdk.android.security.api.AuthorizationManager;
 
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+import org.json.JSONObject;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.MalformedURLException;
+
+public class MainActivity extends Activity implements ResponseListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.adjustTypefaceForMainView();
+
+        TextView buttonText = (TextView) findViewById(R.id.button_text);
+
+        try {
+            //initialize SDK with IBM Bluemix application ID and route
+            //TODO: Please replace <APPLICATION_ROUTE> with a valid ApplicationRoute and <APPLICATION_ID> with a valid ApplicationId
+            //Example:
+            //BMSClient.getInstance().initialize(this, "<APPLICATION_ROUTE>", "<APPLICATION_ID>");
+        }
+        catch (MalformedURLException mue) {
+            this.setStatus(mue.getMessage() + "\nPlease ensure you are using the correct Application Route and Application Id and rebuild the app", false);
+            buttonText.setClickable(false);
+        }
+    }
+
+    public void pingBluemix(View view) {
+        final String buttonWaitColor = "#ff00AED3";
+
+        TextView buttonText = (TextView) findViewById(R.id.button_text);
+        buttonText.setBackgroundColor(Color.parseColor(buttonWaitColor));
+        buttonText.setClickable(false);
+
+        TextView errorText = (TextView) findViewById(R.id.error_text);
+        errorText.setText("");
+
+        AuthorizationManager.getInstance().obtainAuthorizationHeader(this, this);
+    }
+
+    private void adjustTypefaceForMainView () {
+        Typeface IBMFont = Typeface.createFromAsset(getAssets(), "fonts/helvetica-neue-light.ttf");
+
+        TextView errorText = (TextView) findViewById(R.id.error_text);
+        errorText.setTypeface(IBMFont);
+        TextView topText = (TextView) findViewById(R.id.top_text);
+        topText.setTypeface(IBMFont);
+        TextView bottomText = (TextView) findViewById(R.id.bottom_text);
+        bottomText.setTypeface(IBMFont);
+        TextView buttonText = (TextView) findViewById(R.id.button_text);
+        buttonText.setTypeface(IBMFont);
+    }
+
+    private void setStatus(final String text, boolean wasSuccessful){
+        final TextView errorText = (TextView) findViewById(R.id.error_text);
+        final TextView topText = (TextView) findViewById(R.id.top_text);
+        final TextView bottomText = (TextView) findViewById(R.id.bottom_text);
+        final TextView buttonText = (TextView) findViewById(R.id.button_text);
+        final String buttonDefaultColor = "#ff1CB299";
+        final String topStatus = wasSuccessful ? "Yay!" : "Bummer";
+        final String bottomStatus = wasSuccessful ? "You Are Connected" : "Something Went Wrong";
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                buttonText.setBackgroundColor(Color.parseColor(buttonDefaultColor));
+                buttonText.setClickable(true);
+                errorText.setText(text);
+                topText.setText(topStatus);
+                bottomText.setText(bottomStatus);
+            }
+        });
+    }
+
+    @Override
+    public void onSuccess(Response response) {
+        setStatus("", true);
+    }
+
+    @Override
+    public void onFailure(Response response, Throwable throwable, JSONObject jsonObject) {
+        String errorMessage = "Authorization Process Failed With Unknown Error.";
+
+        if (throwable != null) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            throwable.printStackTrace(pw);
+            errorMessage = sw.toString();
+        }
+
+        setStatus(errorMessage, false);
+    }
 }
