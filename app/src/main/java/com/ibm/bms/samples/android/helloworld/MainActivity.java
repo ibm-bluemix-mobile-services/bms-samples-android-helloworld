6f41e16c3c9e58fc651d9b37b5ee454c162c1753
David Cariello <drcariel@us.ibm.com>
2016-04-04 16:17:04 -0400

    Code review updates

--- app/src/main/java/com/ibm/bms/samples/android/helloworld/MainActivity.java
+++ app/src/main/java/com/ibm/bms/samples/android/helloworld/MainActivity.java
@@ -66,54 +66,32 @@ public class MainActivity extends Activity implements ResponseListener{
      */
     public void pingBluemix(View view) {
 
-        TextView buttonText = (TextView) findViewById(R.id.button_text);
+        // Get ping Bluemix button and limit any further clicks until response is received.
+        TextView buttonText = (TextView) view;
         buttonText.setClickable(false);
 
-        TextView errorText = (TextView) findViewById(R.id.error_text);
-        errorText.setText("Attempting to Connect");
+        // Update response text while the response is being processed
+        TextView responseText = (TextView) findViewById(R.id.response_text);
+        responseText.setText("Attempting to Connect");
 
         Log.i(TAG, "Attempting to Connect");
 
-        // Testing the connection to Bluemix by sending a Get request to the Node.js application, using this Activity to handle the response.
-        // This Node.js code was provided in the MobileFirst Services Starter boilerplate.
-        // The below request uses the IBM Mobile First Core sdk to send the request using the applicationRoute that was provided when initializing the BMSClient earlier.
+        // Testing the connection to Bluemix by sending a Get request to the Node.js application. This Node.js code was provided in the MobileFirst Services Starter boilerplate.
+        // The below 'Request' uses the core sdk to send the REST request using the applicationRoute that was provided when initializing the BMSClient earlier.
+        // Since this MainActivity class implements the ResponseListener interface, we can pass 'this' as one of the send() parameters to handle the response.
         new Request(BMSClient.getInstance().getBluemixAppRoute(), Request.GET).send(this, this);
     }
 
-	/**
-     * Updates text fields in the UI
-     * @param messageText String that displays in center text box
-     * @param wasSuccessful Boolean that decides appropriate text to display
-     */
-    private void setStatus(final String messageText, boolean wasSuccessful){
-        final TextView errorText = (TextView) findViewById(R.id.error_text);
-        final TextView topText = (TextView) findViewById(R.id.top_text);
-        final TextView bottomText = (TextView) findViewById(R.id.bottom_text);
-        final TextView buttonText = (TextView) findViewById(R.id.button_text);
-        final String topStatus = wasSuccessful ? "Yay!" : "Bummer";
-        final String bottomStatus = wasSuccessful ? "You Are Connected" : "Something Went Wrong";
-
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                buttonText.setClickable(true);
-                errorText.setText(messageText);
-                topText.setText(topStatus);
-                bottomText.setText(bottomStatus);
-            }
-        });
-    }
-
-    // Implemented for the response listener to handle the success response when Bluemix is pinged
+    // Success handler called when successful response from Bluemix is received
     @Override
     public void onSuccess(Response response) {
         setStatus("", true);
         Log.i(TAG, "Successfully pinged Bluemix!");
     }
 
-    // Implemented for the response listener to handle failure response when Bluemix is pinged
+    // Failure handler called when an unsuccessful response from Bluemix is received
     @Override
-    public void onFailure(Response response, Throwable throwable, JSONObject jsonObject) {
+    public void onFailure(Response response, Throwable throwable, JSONObject extendedInfo) {
         String errorMessage = "";
 
         if (response != null) {
@@ -136,10 +114,40 @@ public class MainActivity extends Activity implements ResponseListener{
             }
         }
 
+        if (extendedInfo != null){
+            errorMessage += "EXTENDED_INFO" + extendedInfo.toString() + "\n";
+        }
+
         if (errorMessage.isEmpty())
             errorMessage = "Request Failed With Unknown Error.";
 
         setStatus(errorMessage, false);
         Log.e(TAG, "Get request to Bluemix failed: " + errorMessage);
     }
+
+	/**
+     * Updates text fields in the UI
+     * @param errorText error String that displays in center text box
+     * @param wasSuccessful Boolean that decides appropriate success vs failure text to display
+     */
+    private void setStatus(final String errorText, boolean wasSuccessful){
+        final TextView responseText = (TextView) findViewById(R.id.response_text);
+        final TextView topText = (TextView) findViewById(R.id.top_text);
+        final TextView bottomText = (TextView) findViewById(R.id.bottom_text);
+        final TextView buttonText = (TextView) findViewById(R.id.button_text);
+        final String topStatus = wasSuccessful ? "Yay!" : "Bummer";
+        final String bottomStatus = wasSuccessful ? "You Are Connected" : "Something Went Wrong";
+
+        // Since this is being done on a separate thread (within success or failure response) we need to run this update on the UI thread or else the app will crash.
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                buttonText.setClickable(true);
+                responseText.setText(errorText);
+                topText.setText(topStatus);
+                bottomText.setText(bottomStatus);
+            }
+        });
+    }
+
 }
